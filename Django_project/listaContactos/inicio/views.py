229e020a077974ebdb588d80927aefe8f0db8947
FabbPP <140754765+FabbPP@users.noreply.github.com>
2024-06-09 15:07:17 -0500

    Templates-agregando hoe.html

--- Django_project/listaContactos/inicio/views.py
+++ Django_project/listaContactos/inicio/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(**args, **kwargs):
+def myHomeView(*args, **kwargs):
     return HttpResponse("<h1>Hola Mundo desde Django</h1>")
+def anotherView(request):
+    return HttpResponse("<h1>Hola, solo otra pagina </h1>")
--- Django_project/listaContactos/listaContactos/urls.py
+++ Django_project/listaContactos/listaContactos/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from inicio.views import myHomeView
+from inicio.views import anotherView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('personas/', include('personas.urls')),
+    path('', myHomeView, name = "Pagina de Inicio"),
+    path('another/', anotherView),
 ]
