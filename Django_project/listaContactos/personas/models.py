eb8587768b6b4cc0ea9d4b6af2c0fb6e85288660
FabbPP <140754765+FabbPP@users.noreply.github.com>
2024-06-09 11:07:56 -0500

    Prueba nuevo campo: Pregunta 1- se agrega el campo

--- Django_project/listaContactos/personas/models.py
+++ Django_project/listaContactos/personas/models.py
@@ -5,4 +5,5 @@ class Persona(models.Model):
     nombres = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
     edad = models.IntegerField() #(max_digits = 3)
+    signo = models.CharField(max_length = 100)
     
