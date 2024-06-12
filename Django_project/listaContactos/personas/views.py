263a5a6c8823e7fb174f30a97f42561052501161
FabbPP <ffpacheco239@gmail.com>
2024-06-12 13:05:12 -0500

    Agregamos una nueva persona

--- Django_project/listaContactos/personas/views.py
+++ Django_project/listaContactos/personas/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Persona
-from django.template import loader
 
 def index(request):
     return HttpResponse("Hello, world. You're at the personas index.")
 # Create your views here.
 personasRegistradas= Persona.objects.all()
 def pruebaView(request):
-    #template = loader.get_template('prueba.html')
     context = {
     'personasRegistradas': personasRegistradas,
     }
-    #return HttpResponse(template.render(context,request))
     return render(request, 'prueba.html', context) 
 
     
