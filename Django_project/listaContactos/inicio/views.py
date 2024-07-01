1e92ba2228beaa68ef6f7886d43ab27abaa261f4
FabbPP <140754765+FabbPP@users.noreply.github.com>
2024-06-30 19:58:02 -0500

    Contexto, uso de tags y variables en el html

--- Django_project/listaContactos/inicio/views.py
+++ Django_project/listaContactos/inicio/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from personas.models import Persona
+personasRegistradas = Persona.objects.all()
 # Create your views here.
 def myHomeView(request,*args, **kwargs):
     print(args,kwargs)
     print(request.user)
-    return render(request,"home.html",{})
+    context = {
+        'personasRegistradas': personasRegistradas
+    }
+    return render(request,"home.html",context)
 def anotherView(request):
     return HttpResponse("<h1>Hola, solo otra pagina</h1>")
