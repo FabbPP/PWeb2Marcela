9287347c26f6aab35e6a9b53170af809496b5b84
FabbPP <140754765+FabbPP@users.noreply.github.com>
2024-07-16 18:38:12 -0500

    Verificar los archivos

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -10,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  name = "Fabiana Pacheco P.";
+  email = "fpachecop@unsa.edu.pe";
+  webpage = "https://www.unsa.edu.pe";
 }
