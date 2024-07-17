d03ea2854270108efe47e5f53c47792d9ff97a31
FabbPP <140754765+FabbPP@users.noreply.github.com>
2024-07-16 21:27:11 -0500

    Generando un componente hello-world - importaciones y componente en el html

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HelloWorldComponent} from "./hello-world/hello-world.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet],
+  imports: [CommonModule,RouterOutlet,HelloWorldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
