5af6758ab7b82a9820c46a1a11954e2d193a166f
FabbPP <140754765+FabbPP@users.noreply.github.com>
2024-07-17 00:04:44 -0500

    Pasando datos al componente user - error no imprime users

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HelloWorldComponent} from "./hello-world/hello-world.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,HelloWorldComponent],
+  imports: [CommonModule,RouterOutlet,HelloWorldComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  user = ["Jaden", "Sergio", "Fabiana", "Aedo"];
+  users = ["Jaden", "Sergio", "Fabiana", "Aedo"];
   activated = false; 
 }
